2122d6323e71fdbc7ba86999ba9c556bcc978fdf
ryannbajaj <82589750+ryannbajaj@users.noreply.github.com>
2021-04-20 17:38:31 +0530

    Add files via upload

---
+++ ryan_library.js
@@ -0,0 +1,27 @@
+function isTouch (obj1, obj2){
+
+    if ( obj1.x- obj2.x < obj1.width/2 +obj2.width/2 &&
+      obj2.x- obj1.x < obj1.width/2 +obj2.width/2 &&
+      obj1.y- obj2.y < obj1.height/2 +obj2.height/2 &&
+      obj2.y- obj1.y < obj1.height/2 +obj2.height/2){
+    
+    return true;
+      }
+  
+    else {
+  return false;
+  
+    }
+  
+  }
+  
+  function bouncingOff(obj1, obj2){
+   if ( obj1.x- obj2.x < obj1.width/2 +obj2.width/2 &&
+    obj2.x- obj1.x < obj1.width/2 +obj2.width/2 &&
+    obj1.y- obj2.y < obj1.height/2 +obj2.height/2 &&
+    obj2.y- obj1.y < obj1.height/2 +obj2.height/2){
+  
+  obj1.velocityX = obj1.velocityX * -1
+  obj1.velocityY = obj1.velocityY * -1
+    }
+  }
---
+++ sketch.js
@@ -0,0 +1,40 @@
+var fixedRect, movingRect, wall;
+
+
+
+
+function setup() {
+  createCanvas(800,400);   
+  //createSprite(400, 200, 50, 50);
+fixedRect = createSprite(400,200,60,60);
+movingRect = createSprite(100,200,30,30);
+wall = createSprite(0,200,1,400);
+fixedRect.debug=true
+movingRect.debug=true
+movingRect.velocityX = 4
+}
+
+function draw() {
+  background("black");  
+ 
+ bouncingOff(movingRect, fixedRect);
+ 
+ 
+  
+
+
+
+
+if(isTouch(movingRect, wall))
+  
+  {
+
+    movingRect.velocityX = 0
+    movingRect.shapeColor = "green"
+
+}
+
+
+drawSprites();
+}
+
